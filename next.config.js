0af8a439af19cd9edd60422bfb14cc6f74d1fd22
Bennyang0419 <7winnie.haku@gmail.com>
2026-04-14 15:23:28 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-const removeImports = require('next-remove-imports')();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // 讓 Markdown 編輯器可以順利被編譯
+  /* 設定預留空間 */
 };
 
-module.exports = removeImports(nextConfig);
+module.exports = nextConfig;
